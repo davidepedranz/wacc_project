c110c3ded21005543936305b67f44b57225bef1d
Davide Pedranz <davide.pedranz@gmail.com>
2017-10-01 22:48:57 +0200

    Fix build

--- frontend/src/app/components/button/button.component.ts
+++ frontend/src/app/components/button/button.component.ts
@@ -1,18 +1,16 @@
-import {Component} from "@angular/core";
-
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'Button',
     host: {
-        '(onclick)':'do_action()'
+        '(onclick)': 'do_action()'
     },
-    templateUrl : './button.component.html',
-    styleUrls: ['./button.component.css'] 
+    templateUrl: './button.component.html',
+    styleUrls: ['./button.component.css']
 })
 
-export class ButtonComponent{
-    do_action(number: string ){
-        alert('function '+number+' processed')
+export class ButtonComponent {
+    do_action(number: number) {
+        alert('function ' + number + ' processed');
     }
-
 }
