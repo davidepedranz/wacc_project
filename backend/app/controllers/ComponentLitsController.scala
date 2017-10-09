b6ae91d09b19dc8f63ec50b22946fdaebd3d64ac
FrancescoSegala <francesco.segala10@gmail.com>
2017-10-09 22:47:07 +0200

    http get e post with docker API

--- backend/app/controllers/ComponentLitsController.scala
+++ backend/app/controllers/ComponentLitsController.scala
@@ -19,8 +19,6 @@ import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
 import akka.util.ByteString
 
-import scala.concurrent.ExecutionContext
-
 import sys.process._
 
 
@@ -35,35 +33,43 @@ class Application @Inject()(ws: WSClient) extends Controller {
 
 @Singleton
 class ComponentListController @Inject()(ws : WSClient) extends Controller {
-  //TODO option -a for see every container, not only the running ones
-  val containersGetResult = "curl --unix-socket /var/run/docker.sock http:/containers/json?all=true" !!
-  val imagesGet = "curl --unix-socket /var/run/docker.sock http:/images/json" !! 
+  
+  //val containersGetResult = "curl --unix-socket /var/run/docker.sock http:/containers/json?all=true" !!
+  //val imagesGet = "curl --unix-socket /var/run/docker.sock http:/images/json" !! 
+  val host = "http://localhost:2375"
+  val getContainersUrl = "/containers/json"
+  val getContainersAllUrl = "/containers/json?all=true"
+  val getImagesUrl = "/images/json"
 
-
-
-  def getContainers = Action{
-    request => 
-      Ok(containersGetResult)
+  def getContainers = Action{ 
+  val res = scala.io.Source.fromURL(host+getContainersUrl).mkString;
+  Ok(res)
     //the response of this:curl --unix-socket /var/run/docker.sock http:/v1.24/containers/json
+  }
+
+  def getContainersAll = Action{
+    request => Ok(scala.io.Source.fromURL(host + getContainersAllUrl).mkString)
   }
 
   def getImages = Action {
     request => 
-    Ok( imagesGet )
-  }
+    Ok(scala.io.Source.fromURL(host + getImagesUrl).mkString)
+  } 
 
 
   def postContainerStop(id: String) = Action {
-    request => 
-    val containerStop = "curl --unix-socket /var/run/docker.sock -X POST http:/v1.24/containers/"+id+"/stop" !!;
+    //request =>
+    
+    val containerStop = "curl  -X POST "+host+"/containers/"+id+"/stop" !!;
     Ok(containerStop)
     }
 
   
   def postContainerStart(id : String ) = Action {
-    request => 
-    val containerStart = "curl --unix-socket /var/run/docker.sock -X POST http:/v1.24/containers/"+id+"/start" !!;
-     Ok(containerStart)
+    //request => 
+    //Ok(host+"/containers"+id+"/start")
+  val containerStart = "curl -X POST "+host+"/containers/"+id+"/start" !!;
+  Ok(containerStart)
   }
    
 }
