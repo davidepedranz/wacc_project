e288d9226d15bbee553a44e1b0f261f1c0f2dc84
FrancescoSegala <francesco.segala10@gmail.com>
2017-10-10 19:51:51 +0200

    added some http call and code cleanup

--- backend/app/controllers/ComponentLitsController.scala
+++ backend/app/controllers/ComponentLitsController.scala
@@ -21,55 +21,48 @@ import akka.util.ByteString
 
 import sys.process._
 
-
-class Application @Inject()(ws: WSClient) extends Controller {
-  val url = "http:/v1.29/containers/json"
-  val response: WSRequest = ws.url(url) 
-
-}
-
 /**/
 
+/**/
 
 @Singleton
 class ComponentListController @Inject()(ws : WSClient) extends Controller {
   
-  //val containersGetResult = "curl --unix-socket /var/run/docker.sock http:/containers/json?all=true" !!
-  //val imagesGet = "curl --unix-socket /var/run/docker.sock http:/images/json" !! 
-  val host = "http://localhost:2375"
+  val host = "http://localhost:2375" //sys.env.get("DOCKER_HOST").mkString
   val getContainersUrl = "/containers/json"
   val getContainersAllUrl = "/containers/json?all=true"
   val getImagesUrl = "/images/json"
-
+  
   def getContainers = Action{ 
   val res = scala.io.Source.fromURL(host+getContainersUrl).mkString;
   Ok(res)
-    //the response of this:curl --unix-socket /var/run/docker.sock http:/v1.24/containers/json
   }
 
   def getContainersAll = Action{
     request => Ok(scala.io.Source.fromURL(host + getContainersAllUrl).mkString)
   }
 
+  def getContainersInfo(id : String ) = Action {
+    request => Ok (scala.io.Source.fromURL(host+"/containers/"+id+"/json").mkString )
+  }
+
   def getImages = Action {
     request => 
-    Ok(scala.io.Source.fromURL(host + getImagesUrl).mkString)
+    Ok( scala.io.Source.fromURL(host + getImagesUrl).mkString )
   } 
 
 
-  def postContainerStop(id: String) = Action {
-    //request =>
-    
+  def postContainerStop(id: String) = Action {    
     val containerStop = "curl  -X POST "+host+"/containers/"+id+"/stop" !!;
-    Ok(containerStop)
+    //Ok(containerStop)
+    redirect("/")
     }
 
   
   def postContainerStart(id : String ) = Action {
-    //request => 
-    //Ok(host+"/containers"+id+"/start")
   val containerStart = "curl -X POST "+host+"/containers/"+id+"/start" !!;
-  Ok(containerStart)
+  //Ok(containerStart)
+  redirect("/")
   }
-   
+
 }
