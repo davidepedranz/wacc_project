dca080c7617e7b5f3ade17dd5c20da0fd4b0e0ed
Davide Pedranz <davide.pedranz@gmail.com>
2017-10-24 11:45:11 +0200

    Fix compilation

--- frontend/src/app/services/service.module.ts
+++ frontend/src/app/services/service.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {ServiceDetailComponent} from './service/service-create-form.component';
+
+import { ServiceDetailComponent } from './service/service-create-form.component';
 
 @NgModule({
-    imports: [
-      CommonModule,
-      FormsModule,
-      ReactiveFormsModule,
-     ],
-     declarations :[
-        ServiceDetailComponent,
-    ]
-  })
-  export class ServiceModule {
-
-  }
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  declarations: [
+    ServiceDetailComponent
+  ]
+})
+export class ServiceModule { }
