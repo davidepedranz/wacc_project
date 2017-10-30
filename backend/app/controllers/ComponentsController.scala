0bb557ae3d97366f4cdc13d3c3ba4dd655d9814b
Davide Pedranz <davide.pedranz@gmail.com>
2017-10-30 10:07:49 +0100

    Add authorizations

--- backend/app/controllers/ComponentsController.scala
+++ backend/app/controllers/ComponentsController.scala
@@ -1,12 +1,10 @@
-
 package controllers
 
 import javax.inject._
 
 import be.objectify.deadbolt.scala.ActionBuilders
 import be.objectify.deadbolt.scala.models.PatternType
-import com.typesafe.config.ConfigFactory
-import models.{Permission, Service, Task, ServiceScale}
+import models.{Permission, Service, ServiceScale, Task}
 import play.api.libs.json._
 import play.api.libs.ws._
 import play.api.mvc._
@@ -15,114 +13,106 @@ import play.api.{Configuration, Logger}
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class ComponentsController @Inject()(implicit ec: ExecutionContext, cc: ControllerComponents, config: Configuration,
-                                     actionBuilders: ActionBuilders, bodyParsers: PlayBodyParsers, ws: WSClient) extends AbstractController(cc) {
+class ComponentsController @Inject()(implicit ec: ExecutionContext, cc: ControllerComponents,
+                                     config: Configuration, actionBuilders: ActionBuilders,
+                                     bodyParsers: PlayBodyParsers, ws: WSClient) extends AbstractController(cc) {
 
-  private val host = ConfigFactory.load().getString("docker_host")
-  private val frontend_host = ConfigFactory.load().getString("frontend_host")
+  // configuration
+  private val host = config.get[String]("docker_host")
+
   private val getContainersUrl = "/containers/json"
   private val getContainersAllUrl = "/containers/json?all=true"
   private val getImagesUrl = "/images/json"
 
   // authorize calls to the Docker Swarm APIs
-  private val servicesReadPermission: actionBuilders.PatternAction.PatternActionBuilder = {
+  private val servicesPermission: actionBuilders.PatternAction.PatternActionBuilder = {
     actionBuilders.PatternAction(value = Permission.SERVICES, patternType = PatternType.EQUALITY)
   }
 
 
-  def getContainers = Action.async {
+  def getContainers: Action[AnyContent] = servicesPermission.defaultHandler() {
     val request = ws.url(host + getContainersUrl)
     request.get().map {
       response => Ok(response.body)
     }
   }
 
-
-  def getContainersAll = Action.async {
+  def getContainersAll: Action[AnyContent] = servicesPermission.defaultHandler() {
     val request = ws.url(host + getContainersAllUrl)
     request.get().map {
       response => Ok(response.body)
     }
   }
 
-  def getContainersInfo(id: String) = Action.async {
+  def getContainersInfo(id: String): Action[AnyContent] = servicesPermission.defaultHandler() {
     val request = ws.url(host + "/containers/" + id + "/json")
     request.get().map {
       response => Ok(response.body)
     }
   }
 
-  def getImages = Action.async {
+  def getImages: Action[AnyContent] = servicesPermission.defaultHandler() {
     val request = ws.url(host + getImagesUrl)
     request.get().map {
       response => Ok(response.body)
     }
   }
 
-
-  def postContainerStop(id: String) = Action.async {
+  def postContainerStop(id: String): Action[AnyContent] = servicesPermission.defaultHandler() {
     val request = ws.url(host + "/containers/" + id + "/stop")
     request.post("").map {
       response => Ok(response.body)
     }
   }
 
-
-  def postContainerStart(id: String) = Action.async {
+  def postContainerStart(id: String): Action[AnyContent] = servicesPermission.defaultHandler() {
     val request = ws.url(host + "/containers/" + id + "/start")
     request.post("").map {
       response => Ok(response.body)
     }
   }
 
-
-  def createService = Action.async(parse.json) {
-    implicit request =>
+  def createService: Action[JsValue] = servicesPermission.apply(parse.json) { request =>
     val bodyAsJson = request.body
     bodyAsJson.validate[ServiceScale] match {
-      case success : JsSuccess[ServiceScale] =>{
-      ws.url(host + "/services/create")
-      .withHttpHeaders("Accept" -> "application/json").post(bodyAsJson).map {
+      case _: JsSuccess[ServiceScale] => ws.url(host + "/services/create")
+        .withHttpHeaders("Accept" -> "application/json").post(bodyAsJson).map {
         response =>
-        response.body contains "ID" match {
-          case true => Ok(response.body)
-          case false => Conflict("Service already exist")
+          //noinspection SimplifyBooleanMatch
+          response.body contains "ID" match {
+            case true => Ok(response.body)
+            case false => Conflict("Service already exist")
           }
-        }
       }
       case JsError(error) => Future(BadRequest(JsError.toJson(error)))
     }
   }
 
-
-  def updateService(id: String) = Action.async(parse.json) { request =>
+  def updateService(id: String): Action[JsValue] = servicesPermission.apply(parse.json) { request =>
     ws.url(host + "/services/" + id)
       .get()
       .flatMap { response =>
         val version: String = Json.stringify((Json.parse(response.body) \ "Version" \ "Index").get)
         request.body.validate[ServiceScale] match {
-          case success : JsSuccess[ServiceScale] => {
-            val updateURL = ws.url(host + "/services/" + id + "/update?version=" + version)
-            updateURL.withHttpHeaders("Accept" -> "application/json").post(request.body).map {
-              response =>
+          case _: JsSuccess[ServiceScale] => ws.url(host + "/services/" + id + "/update?version=" + version)
+            .withHttpHeaders("Accept" -> "application/json").post(request.body).map {
+            response =>
               Ok(response.body)
-            }
           }
           case JsError(error) => Future(BadRequest(JsError.toJson(error)))
         }
       }
   }
 
-  def deleteService(id: String) = Action.async {
-    request =>
-      val deleteServiceURL = ws.url(host + "/services/" + id)
-      deleteServiceURL.execute("DELETE").map {
-        response => Ok(response.body)
-      }
+  def deleteService(id: String): Action[AnyContent] = servicesPermission.defaultHandler() {
+    val deleteServiceURL = ws.url(host + "/services/" + id)
+    deleteServiceURL.execute("DELETE").map {
+      response => Ok(response.body)
+    }
   }
 
   // TODO: cleanup
-  def list: Action[AnyContent] = servicesReadPermission.defaultHandler() {
+  def list: Action[AnyContent] = servicesPermission.defaultHandler() {
 
     val services: Future[Seq[Service]] = ws.url(host + "/services")
       .get()
